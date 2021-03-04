30188a5741bfa72e386b95fd49cbf7bca84ddb88
Fabian <fabian.fritzsche@outlook.com>
2021-03-04 18:45:44 +0100

    header to salzian

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
-	export let name: string;
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1>Welcome to Salzian's website!</h1>
+	<p>
+		Right now this is just a test project for Svelte.
+	</p>
 </main>
 
 <style>
