54448915f4fc7077e66ab6f391fc563fe8c7905e
Fabian <fabian.fritzsche@outlook.com>
2021-03-05 17:23:37 +0100

    simple copy of my github profile i guess

--- src/App.svelte
+++ src/App.svelte
@@ -1,31 +1,57 @@
-<script lang="ts">
-</script>
-
 <main>
-	<h1>Welcome to Salzian's website!</h1>
+	<h1>Welcome to my website!</h1>
+
+	<p>I'm Fabian, I make software and games and stuffs.</p>
+
+	<h2>👨‍💻 What I usually work on</h2>
+
+	<h3>🟦🟧🟥 trivago</h3>
+
+	After my internship, I was lucky to be offered a position as a backend
+	engineer at trivago. There, I now work on a network of microservices for
+	aggregating and delivering data. Everything is built with Kotlin, a language I
+	had to learn from scratch when I started there. Quite the experience with my
+	limited toolset of student knowledge. But also quite the beautiful language 😍
+
+	<h3>Game Development</h3>
+
+	Currently completely paused. I made games for the past 2+ years and burned out
+	pretty hard on them. Working in the confines of a game engine / framework is
+	not always the most rewarding development cycle. And building your own engine?
+	Ain't nobody got time for that!
+
+	<h3>Alternate Universe</h3>
+
+	Alternate Universe is a FiveM freeroam server. I built most of the tools and
+	plugins there. I since decided to step down as the main dev due to the ever
+	increasing work load in a very limited time capacity. I still support the
+	operations of the project and work on some experimental features, but now with
+	a more recreational-oriented time management.
+
+	<h3>Web</h3>
 	<p>
-		Right now this is just a test project for Svelte.
+		I learned Angular in uni, but it was too bloaty for me to make some simple
+		projects. I then focused on React, but really understanding TSX is not easy.
+		Recently I got introduced to Svelte, but that's still a lot of Voodoo magic
+		to me. Let's see where that journey goes...
 	</p>
+	<p>That is also the reason this website looks so shit.</p>
+
+	<h3>Misc</h3>
+
+	I tried to learn some Rust, but the syntax was kinda weird. Still gonna try it
+	at a later date tho, just not in the mood right now.
+
+	<h2>💬 Ask me about</h2>
+
+	<h3>Unity game engine</h3>
+
+	I've worked with Unity during my studies on hobby, university and even a
+	commercial project. I got quite a glimpse of this beast and can get you
+	started learning how to make games. I'm no professional though 😅
+
+	<h2>📫 How to reach me</h2>
+
+	Discord: Salzian#3673<br />
+	Twitter: <a href="https://twitter.com/salzian_dev">@salzian_dev</a>
 </main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
